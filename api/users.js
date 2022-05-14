75e21ad105f7719d09868a0155c26591dd478758
itorr <itorrrrrr@me.com>
2022-05-15 00:33:12 +0800

    test

--- api/users.js
+++ api/users.js
@@ -11,7 +11,7 @@ const get = async uri =>{
 			timeout,
 			responseType:'text',
 			headers:{
-				'user-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36',
+				// 'user-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36',
 				'referer':uri,
 				'Accept-Language': 'zh-CN,zh;q=0.9'
 			}
